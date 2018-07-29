394e8b870877d108d3f29523cbc13144cddaeee7
Kamil Wiśniowski <kamil.wisniowski@icloud.com>
2018-07-29 21:44:06 +0200

    Removing unused libraries

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true]
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
